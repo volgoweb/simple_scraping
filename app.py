aaa540a3798cd8402c6bde8827a72a19fc48c183
Koldunov Dmitry <volgoweb@bk.ru>
2017-08-04 23:57:12 +0300

    add bootstrap

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from celery import Celery
 from flask import jsonify, url_for, render_template
 from scraping import LifehackerCountCommentsParser
+from . import config
 
-app = Flask(__name__, static_url_path='/static', static_folder='static')
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app = Flask(__name__)
+# app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+# app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app.config.from_object(config)
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
@@ -19,13 +21,13 @@ def hello_world():
 def run_scruping():
     task = scraping_lifehacker_task.apply_async()
     data = {
-        'Location': url_for('srcuping_status', task_id=task.id),
+        'Location': url_for('scraping_status', task_id=task.id),
     }
     return jsonify({}), 202, data
 
 
 @app.route('/scraping-status/<task_id>', methods=['POST'])
-def srcuping_status(task_id):
+def scraping_status(task_id):
     task = scraping_lifehacker_task.AsyncResult(task_id)
     data = {
         'state': task.state,
@@ -40,16 +42,6 @@ def srcuping_status(task_id):
 def scraping_lifehacker_task(self):
     parser = LifehackerCountCommentsParser()
     comment_counts = parser.run(update_state_func=self.update_state)
-    # from time import sleep
-    # i = 0
-    # while i < 10:
-    #     i += 1
-    #     sleep(1)
-    #     self.update_state(state='PROGRESS', meta={
-    #         'current': i,
-    #         'total': 100,
-    #         'status': 'Parsing...',
-    #     })
     return {
         'current': 100,
         'total': 100,
---
+++ config.py
@@ -0,0 +1,4 @@
+STATIC_URL_PATH = '/static'
+STATIC_FOLDER = 'static'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
